aa9d799ae2ad2e2e3420991af3e4da8e7212f58c
Dochian Denis <denisdochian.andrei1996@gmail.com>
2024-10-04 13:23:10 +0300

    sters fisier input si adaugat test cases in fisierul rezolvare.py

--- problems/Two Sums/rezolvare.py
+++ problems/Two Sums/rezolvare.py
@@ -8,3 +8,8 @@ class Solution:
 				return [hash_table[target - nums[i]], i]
 			hash_table[nums[i]] = i
 
+
+s = Solution()
+print(s.twoSum([2, 7, 11, 15], 9))
+print(s.twoSum([3, 2, 4], 6))
+print(s.twoSum([3, 3], 6))
