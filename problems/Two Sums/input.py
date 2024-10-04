d79a40b980615b6b2f508ed876563e2a8db9d745
DochianDenis <denisdochian.andrei1996@gmail.com>
2024-10-04 12:07:53 +0300

    Rezolvat

--- problems/Two Sums/input.py
+++ problems/Two Sums/input.py
@@ -1,17 +1,16 @@
-from rezolvare import Solution 
+import rezolvare 
 
 solutie = Solution()
 
 nums = [2, 7, 11, 15]
 target = 9
-print(solutie.twoSum(nums, target))
+print(solutie.twoSum(nums, target)
 
 nums = [3, 2, 4]
 target = 6
-print(solutie.twoSum(nums, target))
+print(solutie.twoSum(nums, target)
 
 nums = [3, 3]
 target = 6
-print(solutie.twoSum(nums, target))
-
+print(solutie.twoSum(nums, target)
 
