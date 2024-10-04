ce6a49d2e3587fe764cc44028046a6307b8a1f78
Dochian Denis <denisdochian.andrei1996@gmail.com>
2024-10-04 12:31:17 +0300

    Adaugat .gitignore si actualizat un fisier care continea o eroare

--- problems/Two Sums/input.py
+++ problems/Two Sums/input.py
@@ -1,16 +1,16 @@
-import rezolvare 
+from rezolvare import Solution
 
 solutie = Solution()
 
 nums = [2, 7, 11, 15]
 target = 9
-print(solutie.twoSum(nums, target)
+print(solutie.twoSum(nums, target))
 
 nums = [3, 2, 4]
 target = 6
-print(solutie.twoSum(nums, target)
+print(solutie.twoSum(nums, target))
 
 nums = [3, 3]
 target = 6
-print(solutie.twoSum(nums, target)
+print(solutie.twoSum(nums, target))
 
